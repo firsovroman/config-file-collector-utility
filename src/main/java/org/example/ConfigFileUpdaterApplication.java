a5282cae7b9ee3811a62faddb34f5c7794c69bd7
Roman <romanfirs@list.ru>
2022-07-13 15:55:21 +0800

    .

--- src/main/java/org/example/ConfigFileUpdaterApplication.java
+++ src/main/java/org/example/ConfigFileUpdaterApplication.java
@@ -6,6 +6,7 @@ import org.example.logic.Processor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ConfigFileUpdaterApplication {
 
@@ -15,6 +16,10 @@ public class ConfigFileUpdaterApplication {
 
         Processor processor = context.getBean("processor" , Processor.class);
         processor.execute();
+
+        System.out.println("Script completed! Press Enter for finish!");
+        Scanner keyboard = new Scanner(System.in);
+        keyboard.nextLine();
 
     }
 }
